36321f774a8cd94fe5d872438a8935ce551b232f
Maria Barragan Estevez <m.barragan.estevez@gmail.com>
2026-07-08 18:31:18 +0200

    Ejercicio5 DOM con html y css

--- Ejercicios-DOM/ejercicio5/script.js
+++ Ejercicios-DOM/ejercicio5/script.js
@@ -1,7 +1,7 @@
 //Ejercicio 5
 
 /*Basandote en el array siguiente, crea una lista ul > li dinámicamente en el html
-que imprima cada uno de los albums. Que tenga la apariencia de una web completa dentro de lo posible.
+que imprima cada uno de los albums. Que tenga la apariencia de una web completa dentro de lo posible.*/
 
 const albums = [
   "De Mysteriis Dom Sathanas",
@@ -9,4 +9,15 @@ const albums = [
   "Ride the Lightning",
   "Painkiller",
   "Iron Fist",
-];*/
+];
+
+const main = document.querySelector("main");
+const ul = document.createElement("ul");
+
+albums.forEach(album => {
+  const li = document.createElement("li");
+  li.textContent = album;
+  ul.appendChild(li);
+});
+
+main.appendChild(ul);
