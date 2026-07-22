8deaf35126a37f406d64d8f61fa203e4de03d127
Maria Barragan Estevez <m.barragan.estevez@gmail.com>
2026-07-22 18:56:10 +0200

    Ejercicio 2

--- ejercicios-es6-asincronia/ejercicio2.js
+++ ejercicios-es6-asincronia/ejercicio2.js
@@ -1,19 +1,44 @@
-/*2.1 Dado el siguiente array, crea una copia usando spread operators.
+//2.1 Dado el siguiente array, crea una copia usando spread operators.
+
 const pointsList = [32, 54, 21, 64, 75, 43]
 
-2.2 Dado el siguiente objeto, crea una copia usando spread operators.
+const copiedPointList = [...pointsList]
+
+console.log(copiedPointList)
+
+//2.2 Dado el siguiente objeto, crea una copia usando spread operators.
+
 const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
 
-2.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando
-spread operatos.
-const pointsList = [32, 54, 21, 64, 75, 43];
+const copiedToy = {...toy};
+
+console.log(copiedToy)
+
+//2.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando spread operatos.
+
+const pointsList1 = [32, 54, 21, 64, 75, 43];
 const pointsLis2 = [54,87,99,65,32];
 
-2.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos
-con spread operators.
-const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const combinedPoint = [...pointsList1, ...pointsLis2];
+
+console.log(combinedPoint)
+
+//2.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos 
+// con spread operators.
+
+const toy1 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
 const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
 
-2.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2
-pero sin editar el array inicial. De nuevo, usando spread operatos.
-const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja']; */
+const combinedToy = {...toy1, ...toyUpdate};
+
+console.log(combinedToy)
+
+//2.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2
+//pero sin editar el array inicial. De nuevo, usando spread operatos.
+
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja']; 
+
+const colors1 = [...colors.slice(0, 2), ...colors.slice(3)];
+
+console.log(colors);
+console.log(colors1);
