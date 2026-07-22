fe01323c4f47619c608cf8574115e21f282ede12
Maria Barragan Estevez <m.barragan.estevez@gmail.com>
2026-07-22 18:38:55 +0200

    Ejercicio1

--- ejercicios-es6-asincronia/EjerciciosES6Asincronia.js
+++ ejercicios-es6-asincronia/EjerciciosES6Asincronia.js
@@ -1,9 +1,11 @@
+//Ejercicios ES6+ y asincronía
+
+//Ejercicio 1
 /*1.1 En base al siguiente javascript, crea variables en base a las propiedades
 del objeto usando object destructuring e imprimelas por consola. Cuidado,
 no hace falta hacer destructuring del array, solo del objeto.*/
 
 const game = {title: 'The Last of Us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
-
 
 const {title, gender, year} = game;
 
@@ -17,12 +19,6 @@ imprimelo por consola.*/
 
 const fruits = ['Banana', 'Strawberry', 'Orange'];
 
-const [fruit1, fruit2, fruit3] = fruits;
-
-console.log(fruit1);
-console.log(fruit2);
-console.log(fruit3);
-
 /*1.3 En base al siguiente javascript, usa destructuring para crear 2
 variables igualandolo a la función e imprimiendolo por consola.*/
 
@@ -30,24 +26,25 @@ const animalFunction = () => {
     return {name: 'Bengal Tiger', race: 'Tiger'} 
 };
 
-const{name, race} = animalFunction();
-
-console.log(name)
-console.log(race)
-
 /*1.4 En base al siguiente javascript, usa destructuring para crear las
 variables name y itv con sus respectivos valores. Posteriormente crea
 3 variables usando igualmente el destructuring para cada uno de los años
 y comprueba que todo esta bien imprimiendolo.*/
 
-const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] } 
+const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
 
-const {name: carName, itv} = car;
+//Ejercicio 2
 
-const [year1, year2, year3] = itv;
+//Ejercicio 3
 
-console.log(carName);
-console.log(itv);
-console.log(year1);
-console.log(year2);
-console.log(year3);
+//Ejercicio 4
+
+//Ejercicio 5
+
+//Ejercicio 6
+
+//Ejercicio 7
+
+//Ejercicio 8
+
+//Ejercicio 9
